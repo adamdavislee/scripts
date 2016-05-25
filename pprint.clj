f1a8293de229d99f54749809a8ead8cbc9e2b119
Adam Lee <adamdavislee@gmail.com>
2016-05-25 02:09:40 +0000

    eureka; it appears to work now

--- pprint.clj
+++ pprint.clj
@@ -3,19 +3,14 @@
 (declare convert-kv)
 (def padding #(apply str (repeat % " ")))
 (def strcat #(->> (apply concat %&) (apply str)))
+(defn my-max-key [x] (if (empty? x) [""] (apply (partial max-key count) x)))
 (defn longest-key [m]
-  (->> m keys (filter atomic?) (map str) (my-max-key count)))
+  (->> m keys (filter atomic?) (map str) my-max-key))
 (def length (comp count str))
 (def not-map? (complement map?))
 (def nested? #(some coll? %))
 (def join-lines (partial str/join "\n"))
-(defn my-max-key
-  ([one-arg] "")
-  ([pred coll] (apply max-key pred coll)))
-(defn convert-vec [depth vec]
-  (conj []
-        (map (partial convert-items (inc depth)) vec)
-        "")) ;;the string will become a newline, because convert calls join-lines
+(defn has-atomic? [coll] (some atomic? coll))
 (defn convert-items
   ([thing] (convert-items -1 thing))
   ([depth thing]
@@ -27,6 +22,10 @@
 
          (map? thing)
          (map (partial convert-kv (inc depth) (longest-key thing)) thing))))
+(defn convert-vec [depth vec]
+  (conj []
+        (map (partial convert-items (inc depth)) vec)
+        ""))
 (defn convert
   ([depth thing]
    (let [converted (convert-items depth thing)]
@@ -35,15 +34,15 @@
        converted)))
   ([thing] (convert -1 thing)))
 (defn string-vertically [depth [key value]]
-  (str (padding depth) (convert key) "\n"
-       (padding depth) "↓" "\n"
-       (padding depth) (convert value) "\n"))
+  (str  (convert depth key) "\n"
+        (convert (inc depth) "↓") "\n"
+        (convert depth value) "\n"))
+(defn diff-key-lengths [key1 key2]
+  (- (length key1) (length key2)))
 (defn string-horizontally [depth longest-key [key value]]
-  (let [value-align-padding (- (length longest-key) (length key))]
-    (str (padding depth) key " → " (padding value-align-padding) value)))
+  (str (padding depth) key (padding (diff-key-lengths longest-key key)) " → " value))
 (defn convert-kv [depth longest-key [key value]]
   (if (nested? [key value])
-    (string-vertically (inc depth) [key value])
+    (string-vertically depth [key value])
     (string-horizontally depth longest-key [key value])))
-(->> "not-pprinted.clj" slurp read-string convert (spit "pprinted"))
-(max-key count (map str '()))
+(->> "../not-pprinted.clj" slurp read-string convert (spit "../pprinted"))
