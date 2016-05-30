97735b7ddc5c21edd4e42f253f3626779eaf8c12
Adam Lee <adamdavislee@gmail.com>
2016-05-30 03:58:14 +0000

    refactored pprint.clj to use closures, removing repetive depth and thing parameters

--- pprint.clj
+++ pprint.clj
@@ -1,5 +1,6 @@
 (def atomic? (complement coll?))
 (def padding #(apply str (repeat % " ")))
+(def tabulate #(apply str (repeat % "\t")))
 (def strcat #(->> (apply concat %&) (apply str)))
 (defn my-max-key [x] (if (empty? x) [""] (apply (partial max-key count) x)))
 (defn longest-key [m] (->> m keys (filter atomic?) (map str) my-max-key))
@@ -8,35 +9,44 @@
 (def nested? #(some coll? %))
 (def join-lines (partial clojure.string/join "\n"))
 (defn has-atomic? [coll] (some atomic? coll))
-(declare convert-items convert)
-(defn convert-seq [depth vec] (conj []
-                                    (map (partial convert-items (inc depth)) vec)
-                                    ""))
-(defn string-vertically [depth [key value]]
-  (str (convert depth key) "\n"
-       (convert (inc depth) "↓") "\n"
-       (convert depth value) "\n"))
-(defn diff-key-lengths [key1 key2]
-  (- (length key1) (length key2)))
-(defn string-horizontally [depth longest-key [key value]]
-  (str (padding depth) key (padding (diff-key-lengths longest-key key)) " → " value))
-(defn convert-kv [depth longest-key [key value]]
-  (if (nested? [key value])
-    (string-vertically depth [key value])
-    (string-horizontally depth longest-key [key value])))
-(defn convert-items
-  ([depth thing]
-   (cond (atomic? thing)
-         [(str (padding depth) thing)]
-
-         (not-map? thing)
-         (convert-seq depth thing)
-
-         (map? thing)
-         (map (partial convert-kv (inc depth) (longest-key thing)) thing)))
-  ([thing] (convert-items -1 thing)))
+(defn diff-key-lengths [key1 key2] (- (length key1) (length key2)))
 (defn convert
-  ([depth thing] (->> thing (convert-items depth) flatten join-lines))
-  ([thing] (convert -1 thing)))
-(def sample-input [:first {:k :v {:sample :nested :map :test :points :to} {:this :other :nice :map}}])
+  ([thing] (convert -1 thing))
+  ([depth thing]
+   (defn convert-items []
+     (defn convert-seq []
+       (conj []
+             (map (partial convert (inc depth)) thing)
+             ""))
+     (defn string-horizontally [[key value]]
+       (str (tabulate (inc depth))
+            key
+            (padding (diff-key-lengths (longest-key thing) key))
+            " → "
+            value))
+     (defn string-vertically [[key value]]
+       (str (convert (inc depth) key) "\n"
+            (convert (+ 2 depth) "↓") "\n"
+            (convert (inc depth) value) "\n"))
+     (defn convert-kv [[key value]]
+       (if (nested? [key value])
+           (string-vertically [key value])
+           (string-horizontally [key value])))
+     (cond (atomic? thing)
+           [(str (tabulate depth) thing)]
+
+           (not-map? thing)
+           (convert-seq)
+
+           (map? thing)
+           (map convert-kv thing)))
+   (->>  (convert-items) flatten join-lines)))
+
+(def sample-input [["the first thing in this nested vector"]
+                   {{"this is a key in a nested map"
+                    "that points to me!!!"}
+                   {"and that entire map points to this map!!!"
+                    "cool!!!"
+                    "but it gets cooler cause..."
+                    "the value's line up!!!"}}])
 (->> sample-input convert println)
